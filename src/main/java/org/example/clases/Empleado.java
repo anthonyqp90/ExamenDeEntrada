990f3ca735ba49d76d93283ae08e74aeb45d17d0
anthony quispe perez <krishqp90@gmail.com>
2024-04-02 21:30:24 -0500

    add metodos constructor,getter,setter en la clase Empleado

--- src/main/java/org/example/clases/Empleado.java
+++ src/main/java/org/example/clases/Empleado.java
@@ -1,10 +1,29 @@
 package org.example.clases;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Empleado {
     private int id;
     private String nombre;
     private String departamento;
     private double salario;
 
-    //Constructor
-}
+    //METODO CONSTRUCTOR
+        public Empleado(int id,String nombre,String departamento,double salario){
+            this.id=id;
+            this.nombre=nombre;
+            this.departamento=departamento;
+            this.salario=salario;
+        }
+     //METODOS ABSTRACTOS
+    public void mostrarDetalles(){
+
+    }
+    public void calcularBono(){
+
+    }
+
+    }
+
