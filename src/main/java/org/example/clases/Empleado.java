57f21b6f1e95841eeb829d685ef8986123a5453d
anthony quispe perez <krishqp90@gmail.com>
2024-04-02 21:05:34 -0500

    add clases , interface and dependencia lombok

---
+++ src/main/java/org/example/clases/Empleado.java
@@ -0,0 +1,10 @@
+package org.example.clases;
+
+public class Empleado {
+    private int id;
+    private String nombre;
+    private String departamento;
+    private double salario;
+
+    //Constructor
+}
